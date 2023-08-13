cfdc0e372d73b4db0bc9859fcf9968f37da1dbf1
Emily McMahon <emilyemcmahonn@gmail.om>
2023-08-13 15:52:04 -0700

    put connect function in client file, exported to play

---
+++ client.js
@@ -0,0 +1,32 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // set a message for connection 
+  conn.on('connect', () => {
+    console.log('Connected to the server! 🐍');
+  });
+
+  // hanlde incoming data
+  conn.on('data', (data) => {
+    console.log(data.toString());
+    conn.end();
+  });
+
+  // set a message for disconnection 
+  conn.on('end', () => {
+    console.log('Disconnected from server 👋');
+  }); 
+
+
+  return conn;
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,34 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  // set a message for connection 
-  conn.on('connect', () => {
-    console.log('Connected to the server! 🐍');
-  });
-
-  // hanlde incoming data
-  conn.on('data', (data) => {
-    console.log(data.toString());
-    conn.end();
-  });
-
-  // set a message for disconnection 
-  conn.on('end', () => {
-    console.log('Disconnected from server 👋');
-  }); 
-
-
-  return conn;
-};
+const connect = require('../snake-client/client');
 
 console.log("Connecting ...");
 connect();
